a69d55a43ccf2245bd054801ff2fbdce49de0e3a
Expedito <expeditopaivaneto@gmail.com>
2023-11-20 23:18:53 -0300

    Criando componente de search

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,19 +4,22 @@ import { CommonModule } from '@angular/common';
 //Components
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SearchComponent } from './search/search.component';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SearchComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    SearchComponent
   ]
 })
 export class SharedModule { }
